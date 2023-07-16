c1ac7b1fe6f2bb704207dc77f6f2385dcb4dd12b
kuku1198 <kuku1198@gmail.com>
2023-07-16 18:14:06 +0900

    first commit

---
+++ src/main/java/inflearnholyeyehellospring/inflearnholyeyehellospring/InflearnHolyeyeHelloSpringApplication.java
@@ -0,0 +1,13 @@
+package inflearnholyeyehellospring.inflearnholyeyehellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnHolyeyeHelloSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnHolyeyeHelloSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/inflearnholyeyehellospring/inflearnholyeyehellospring/InflearnHolyeyeHelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package inflearnholyeyehellospring.inflearnholyeyehellospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnHolyeyeHelloSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
