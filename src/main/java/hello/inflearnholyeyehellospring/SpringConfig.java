69c0cf20a2e3d664952c209efb7784ab6cc52c28
kuku1198 <kuku1198@gmail.com>
2024-01-28 19:19:17 +0900

    스프링 DB 접근 기술 #2

--- src/main/java/hello/inflearnholyeyehellospring/SpringConfig.java
+++ src/main/java/hello/inflearnholyeyehellospring/SpringConfig.java
@@ -1,13 +1,21 @@
 package hello.inflearnholyeyehellospring;
 
+import hello.inflearnholyeyehellospring.repository.JdbcMemberRepository;
 import hello.inflearnholyeyehellospring.repository.MemberRepository;
 import hello.inflearnholyeyehellospring.repository.MemoryMemberRepository;
 import hello.inflearnholyeyehellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +24,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
